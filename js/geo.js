cb097abba13e6690f67e2ff6e4ccc72d52e076af
manu <manumq30@gmail.com>
2026-02-06 16:51:46 -0300

    removed dev_mode from geo.js, need it up and running in gh pages

--- js/geo.js
+++ js/geo.js
@@ -1,20 +1,20 @@
-import { DEV_MODE, devCoords } from '../dev.js';
+//import { DEV_MODE, devCoords } from '../dev.js';
 
 export function getPosition() {
-  if (DEV_MODE) {
-    return Promise.resolve(devCoords());
-  }
+	//if (DEV_MODE) {
+	//  return Promise.resolve(devCoords());
+	//}
 
-  return new Promise((resolve, reject) => {
-    if (!navigator.geolocation) {
-      reject(new Error('Geolocation not supported'));
-      return;
-    }
+	return new Promise((resolve, reject) => {
+		if (!navigator.geolocation) {
+			reject(new Error('Geolocation not supported'));
+			return;
+		}
 
-    navigator.geolocation.getCurrentPosition(
-      (pos) => resolve({ lat: pos.coords.latitude, lon: pos.coords.longitude }),
-      (err) => reject(err),
-      { enableHighAccuracy: false, timeout: 10000 }
-    );
-  });
+		navigator.geolocation.getCurrentPosition(
+			(pos) => resolve({ lat: pos.coords.latitude, lon: pos.coords.longitude }),
+			(err) => reject(err),
+			{ enableHighAccuracy: false, timeout: 10000 }
+		);
+	});
 }
